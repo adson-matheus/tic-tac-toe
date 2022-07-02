c7ca4be5fc942e9b11ae4db3f0a2c7109054037e
Rauan Meirelles <77637967+rauan-meirelles@users.noreply.github.com>
2022-07-02 16:04:53 -0300

    Implementando escolha do vencedor #4
    Co-Authored-By: Wesley de Morais <37916229+WesleyVitor@users.noreply.github.com>
    Co-Authored-By: Adson Matheus <40866921+adson-matheus@users.noreply.github.com>

--- lib/view/tabuleiro.dart
+++ lib/view/tabuleiro.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tic_tac_toe/viewModel/jogo_da_velha.dart';
 
 import 'button.dart';
 
@@ -12,23 +13,42 @@ class Tabuleiro extends StatefulWidget {
 class _TabuleiroState extends State<Tabuleiro> {
   @override
   Widget build(BuildContext context) {
-    return GridView.count(
-      primary: false,
-      padding: const EdgeInsets.all(20),
-      crossAxisSpacing: 10,
-      mainAxisSpacing: 10,
-      crossAxisCount: 3,
-      children: const <Widget>[
-        StyledButton(posicao: 0),
-        StyledButton(posicao: 1),
-        StyledButton(posicao: 2),
-        StyledButton(posicao: 3),
-        StyledButton(posicao: 4),
-        StyledButton(posicao: 5),
-        StyledButton(posicao: 6),
-        StyledButton(posicao: 7),
-        StyledButton(posicao: 8),
-      ],
+    return AnimatedBuilder(
+      animation: JogoDaVelha.instance,
+      builder: (context, child) {
+        return GridView.count(
+          primary: false,
+          padding: const EdgeInsets.all(20),
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+          crossAxisCount: 3,
+          children: <Widget>[
+            const Text(''),
+            Center(
+              child: Text(
+                  JogoDaVelha.instance.venceu == false
+                      ? JogoDaVelha.instance.vez == 0
+                          ? 'Vez do jogador 0'
+                          : 'Vez do jogador 1'
+                      : 'Vencedor: Jogador ${JogoDaVelha.instance.vez}',
+                  style: const TextStyle(
+                    fontSize: 28.0,
+                    fontWeight: FontWeight.bold,
+                  )),
+            ),
+            const Text(''),
+            const StyledButton(posicao: 0),
+            const StyledButton(posicao: 1),
+            const StyledButton(posicao: 2),
+            const StyledButton(posicao: 3),
+            const StyledButton(posicao: 4),
+            const StyledButton(posicao: 5),
+            const StyledButton(posicao: 6),
+            const StyledButton(posicao: 7),
+            const StyledButton(posicao: 8),
+          ],
+        );
+      },
     );
   }
 }
--- lib/viewModel/jogo_da_velha.dart
+++ lib/viewModel/jogo_da_velha.dart
@@ -1,11 +1,10 @@
-
-
 import 'package:flutter/material.dart';
 
 class JogoDaVelha extends ChangeNotifier {
   static JogoDaVelha instance = JogoDaVelha();
   int vez = 0;
   bool venceu = false;
+
   List<String> valores = ['', '', '', '', '', '', '', '', ''];
   List<bool> marcado = [
     false,
@@ -24,32 +23,32 @@ class JogoDaVelha extends ChangeNotifier {
       instance.vez == 0
           ? instance.valores[posicao] = 'X'
           : instance.valores[posicao] = 'O';
+      vencedor();
       instance.vez = instance.vez == 0 ? 1 : 0;
       instance.marcado[posicao] = true;
-      
       
       notifyListeners();
     }
   }
 
-  void vencedor(){
-    
+  void vencedor() {
     const posicoesVencedoras = [
-      [0,1,2],
-      [3,4,5],
-      [6,7,8],
-      [0,4,8],
-      [2,4,6],
-      [0,3,6],
-      [1,4,7],
-      [2,5,8]
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8]
     ];
 
-    for(List<int> linha in posicoesVencedoras){
-      if((instance.valores[linha[0]] == instance.valores[linha[1]]) && 
-      (instance.valores[linha[0]] == instance.valores[linha[2]] && instance.valores[linha[0]] != '')){
-          instance.venceu = true;
-          break;
+    for (List<int> linha in posicoesVencedoras) {
+      if ((instance.valores[linha[0]] == instance.valores[linha[1]]) &&
+          (instance.valores[linha[0]] == instance.valores[linha[2]] &&
+              instance.valores[linha[0]] != '')) {
+        instance.venceu = true;
+        break;
       }
     }
   }
