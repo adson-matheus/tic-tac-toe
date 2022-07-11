8a034588d4c93647acd68ef2bc1281c312f25aac
Adson Matheus <adson.matheus62@gmail.com>
2022-07-11 11:25:51 -0300

    feat: restore

--- lib/viewModel/snapshot.dart
+++ lib/viewModel/snapshot.dart
@@ -1,24 +1,28 @@
 import 'jogo_da_velha.dart';
 
 class SnapShot {
+  JogoDaVelha jogoDaVelha;
   int vez;
   bool venceu;
   bool terminou;
   List<String> valores;
   List<bool> marcado;
+
   SnapShot({
+    required this.jogoDaVelha,
     required this.vez,
     required this.venceu,
     required this.terminou,
     required this.marcado,
-    required this.valores
+    required this.valores,
   });
 
-  void restore(){
-    JogoDaVelha.instance.vez = vez;
-    JogoDaVelha.instance.venceu = venceu;
-    JogoDaVelha.instance.terminou = terminou;
-    JogoDaVelha.instance.valores = valores;
-    JogoDaVelha.instance.marcado = marcado;
+  void restore() {
+    jogoDaVelha.vez = vez;
+    jogoDaVelha.venceu = venceu;
+    jogoDaVelha.terminou = terminou;
+    jogoDaVelha.valores = valores;
+    jogoDaVelha.marcado = marcado;
+    JogoDaVelha.instance = jogoDaVelha;
   }
 }
